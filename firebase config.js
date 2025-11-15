b65312a714cbb81652e21758b50f1833fb0d7437
LadyK-AI <kelesetlogelo@gmail.com>
2025-11-15 17:55:41 +0200

    firebase config.js

--- firebase config.js
+++ firebase config.js
@@ -9,14 +9,13 @@
 // Initialize Firebase
 try {
 const firebaseConfig = {
-    apiKey: "AIzaSyCtx_K76mH3agerccTXMc08W8sqKXUI_pA",
-    authDomain: "wishing-well-2025.firebaseapp.com",
-    databaseURL:"https://wishing-well-2025-default-rtdb.firebaseio.com",
-    projectId: "wishing-well-2025",
-    storageBucket: "wishing-well-2025.firebasestorage.app",
-    messagingSenderId: "643053862807",
-    appId: "1:643053862807:web:54ddb442035fda90ce8ed2"  
-
+     apiKey: "AIzaSyCtx_K76mH3agerccTXMc08W8sqKXUI_pA",
+  authDomain: "wishing-well-2025.firebaseapp.com",
+  databaseURL: "https://wishing-well-2025-default-rtdb.firebaseio.com",
+  projectId: "wishing-well-2025",
+  storageBucket: "wishing-well-2025.firebasestorage.app",
+  messagingSenderId: "643053862807",
+  appId: "1:643053862807:web:54ddb442035fda90ce8ed2"
 };
 
 // Initialize app + database
@@ -31,3 +30,4 @@ const firebaseConfig = {
 } catch (err) {
   console.error("🔥 Firebase init error (check config):", err);
 }
+
